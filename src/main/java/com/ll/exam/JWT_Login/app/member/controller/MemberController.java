b1bae32a56fdb5f4f5b2d891991e2e99c111e29d
icoo08217 <icoo08216@gmail.com>
2022-10-05 16:18:24 +0900

    Test Case 3,4 추가

--- src/main/java/com/ll/exam/JWT_Login/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/JWT_Login/app/member/controller/MemberController.java
@@ -1,23 +1,63 @@
 package com.ll.exam.JWT_Login.app.member.controller;
 
+import com.ll.exam.JWT_Login.app.member.entity.Member;
+import com.ll.exam.JWT_Login.app.member.service.MemberService;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import static org.springframework.http.HttpStatus.*;
+
 @RestController
-@RequestMapping("/member")
+@RequestMapping(value = "/member")
+@RequiredArgsConstructor
 public class MemberController {
 
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
     @PostMapping("/login")
-    public String login(@RequestBody LoginDto loginDto) {
-        return "username : %s , password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+
+        if (loginDto.isNotValid()) {
+            return new ResponseEntity<>(null, null, BAD_REQUEST);
+        }
+
+        Member member = memberService.findByUsername(loginDto.getUsername()).orElse(null);
+
+        if (member == null) {
+            return new ResponseEntity<>(null, null, BAD_REQUEST);
+        }
+
+        if (passwordEncoder.matches(loginDto.getPassword(), member.getPassword()) == false) {
+            return new ResponseEntity<>(null, null, BAD_REQUEST);
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authentication" , loginDto.getUsername());
+
+        String body = "username : %s , password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+
+        return new ResponseEntity<>(body , headers , OK);
     }
 
     @Data
     public static class LoginDto{
         private String username;
         private String password;
+
+        public boolean isNotValid() {
+            if (username == null || password == null ||username.trim().length() == 0 || password.trim().length() == 0){
+                return false;
+            }
+            return true;
+        }
     }
 }
--- src/test/java/com/ll/exam/JWT_Login/AuthTests.java
+++ src/test/java/com/ll/exam/JWT_Login/AuthTests.java
@@ -1,18 +1,22 @@
 package com.ll.exam.JWT_Login;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.charset.StandardCharsets;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -48,4 +52,110 @@ class AuthTests {
                 .andExpect(status().is2xxSuccessful());
     }
 
+    @Test
+    @DisplayName("POST /member/login 으로 올바른 username과 password를 넘기면 JWT토큰을 발급해준다.")
+    void Test2() throws Exception {
+        // When
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/member/login")
+                                .content("""
+                                        {
+                                            "username": "user1",
+                                            "password": "1234"
+                                        }
+                                        """.stripIndent())
+                                .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+                )
+                .andDo(print());
+
+        // Then
+        resultActions
+                .andExpect(status().is2xxSuccessful());
+
+        MvcResult mvcResult = resultActions.andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        String authentication = response.getHeader("Authentication");
+        assertThat(authentication).isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("POST /member/login 호출할 때 username 이나 password 를 누락하면 400")
+    void Test3() throws Exception {
+        // When
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/member/login")
+                                .content("""
+                                        {
+                                            "username": "",
+                                            "password": "1234"
+                                        }
+                                        """.stripIndent())
+                                .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+                )
+                .andDo(print());
+
+        // Then
+        resultActions
+                .andExpect(status().is4xxClientError());
+
+        resultActions = mvc
+                .perform(
+                        post("/member/login")
+                                .content("""
+                                        {
+                                            "username": "user1",
+                                            "password": ""
+                                        }
+                                        """.stripIndent())
+                                .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+                )
+                .andDo(print());
+
+        // Then
+        resultActions
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @DisplayName("POST /member/login 호출할 때 올바르지 않는 username 이나 password 를 입력하면 400")
+    void Test4() throws Exception {
+        // When
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/member/login")
+                                // user3 은 없는 사용자
+                                .content("""
+                                        {
+                                            "username": "user3",
+                                            "password": "1234"
+                                        }
+                                        """.stripIndent())
+                                .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+                )
+                .andDo(print());
+
+        // Then
+        resultActions
+                .andExpect(status().is4xxClientError());
+
+        resultActions = mvc
+                .perform(
+                        post("/member/login")
+                                // user1의 비밀번호는 1234이다
+                                .content("""
+                                        {
+                                            "username": "user1",
+                                            "password": "12345"
+                                        }
+                                        """.stripIndent())
+                                .contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+                )
+                .andDo(print());
+
+        // Then
+        resultActions
+                .andExpect(status().is4xxClientError());
+    }
 }
